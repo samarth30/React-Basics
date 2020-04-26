3a46d257440d5f15c548443d45e1410c81fa4998
samarth <samarth.gugnani30@gmail.com>
2020-04-26 19:26:22 +0530

    just making the app

---
+++ basics/src/components/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function about() {
+    return (
+        <React.Fragment>
+            <h1>about</h1>
+            <p>this is a todo list app v1.0.0 it is part of react crash course</p>
+        </React.Fragment>
+    )
+}
+
+export default about;
